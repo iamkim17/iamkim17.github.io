27c929c7a57e661727271df382cc0c2e98ebe3e1
kimvasquez <vkim1217@gmail.com>
2022-06-08 14:16:11 +0800

    Initial Commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Hymnkey-Fort!fy Demo-0.1";
+const contentToCache = [
+    "Build/fortifybuild.loader.js",
+    "Build/fortifybuild.framework.js.unityweb",
+    "Build/fortifybuild.data.unityweb",
+    "Build/fortifybuild.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
