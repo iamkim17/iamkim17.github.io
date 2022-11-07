c4c99d4c06dd117c2dc546efb2d7a4b35e81fa36
Kim Dominique Vasquez <vkim1217@gmail.com>
2022-11-07 13:02:42 +0800

    new mictest4

---
+++ mictest4/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Boomtech-MicWebGLNew2-1.0";
+const contentToCache = [
+    "Build/mictest4.loader.js",
+    "Build/mictest4.framework.js.unityweb",
+    "Build/mictest4.data.unityweb",
+    "Build/mictest4.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
